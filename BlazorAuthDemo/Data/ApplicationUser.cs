9d96a0725ca1fa56bddd5564bf3ba58b699097cc
Jeelsavaliya <jeelsavaliya007@gmail.com>
2024-10-07 15:27:36 +0530

    Add project files.

---
+++ BlazorAuthDemo/Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorAuthDemo.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+       // public string? Role { get; set; }
+
+
+    }
+
+}
---
+++ BlazorAuthDemo/Services/MailService.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Mail;
+using BlazorAuthDemo.Data;
+
+namespace BlazorAuthDemo.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(string email, string subject, string htmlMessage);
+    }
+
+    public class MailService : IMailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //Email verification
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("jeelsavaliya007@gmail.com");
+                message.To.Add(email);
+                message.Subject = "Confirm your email";
+                message.IsBodyHtml = true;
+                message.Body = htmlMessage;
+
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host
+
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("jeelsavaliya007@gmail.com", "jkqw rnhk kdfv crwh"); //password is create from gmail security
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
